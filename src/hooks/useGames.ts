4998bff2dcc3512ecb43e1558e30f376a12068f5
Alain <davisbedetse@gmail.com>
2026-03-24 09:02:03 -0400

    Building icon list

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,17 +2,25 @@ import { useState, useEffect } from "react";
 import apiClient from "../services/api-client";
 import { CanceledError } from "axios";
 
+export interface Platform {
+  id: number;
+  name: string;
+  slug: string;
+}
+
+export interface Game {
+  id: number;
+  name: string;
+  background_image: string;
+  parent_platforms: { platform: Platform }[];
+}
+
+interface FetchGamesResponse {
+  count: number;
+  results: Game[];
+}
+
 const useGames = () => {
-  interface Game {
-    id: number;
-    name: string;
-  }
-
-  interface FetchGamesResponse {
-    count: number;
-    results: Game[];
-  }
-
   const [games, setGames] = useState<Game[]>([]);
   const [error, setError] = useState("");
 
