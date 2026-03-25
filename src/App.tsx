590331221492ab06284d10c7e7bbc805453d6294
Alain <davisbedetse@gmail.com>
2026-03-25 13:58:32 -0400

    Optimizing images

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ export interface GameQuery {
   genre: Genre | null;
   platform: Platform | null;
   sort: Sort | null;
+  searchText: string;
 }
 
 function App() {
@@ -33,7 +34,9 @@ function App() {
       // }}
     >
       <GridItem area="nav">
-        <NavBar />
+        <NavBar
+          onSearch={(searchText) => setGameQuery({ ...gameQuery, searchText })}
+        />
       </GridItem>
 
       <GridItem area="aside" paddingX="10px">
